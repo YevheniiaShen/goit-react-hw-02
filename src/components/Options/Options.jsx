4207f5246ccbe64a12b9e8cb3b6488c05590fde8
Евгения Додиван <evgenita.d97@gmail.com>
2024-02-08 23:10:14 +0100

    try fix

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,38 +1,38 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// export const Options = () => {
-//     const [good, setGood] = useState(0);
+export const Options = () => {
+    const [good, setGood] = useState(0);
 
-//     const[neutral, setNeutral] = useState(0);
+    const[neutral, setNeutral] = useState(0);
 
-//     const [bad, setBad] = useState(0);
+    const [bad, setBad] = useState(0);
 
-//     const handleClickGood = () => {
-//         setGood(good + 1);
-//     };
+    const handleClickGood = () => {
+        setGood(good + 1);
+    };
 
-//     const handleClickNeutral = () => {
-//         setNeutral(neutral + 1);
-//     };
+    const handleClickNeutral = () => {
+        setNeutral(neutral + 1);
+    };
 
-//     const handleClickBad = () => {
-//         setBad(bad + 1);
-//     };
+    const handleClickBad = () => {
+        setBad(bad + 1);
+    };
   
-//     return (
-//         <div>
-//           <ul>
-//             <li>
-//               <button onClick={handleClickGood}>Good {good}</button>
-//             </li>
-//             <li>
-//               <button onClick={handleClickNeutral}>Neutral {neutral}</button>
-//             </li>
-//             <li>
-//               <button onClick={handleClickBad}>Bad {bad}</button>
-//             </li>
-//           </ul>
-//         </div>
-//       );
-//     };
+    return (
+        <div>
+          <ul>
+            <li>
+              <button onClick={handleClickGood}>Good {good}</button>
+            </li>
+            <li>
+              <button onClick={handleClickNeutral}>Neutral {neutral}</button>
+            </li>
+            <li>
+              <button onClick={handleClickBad}>Bad {bad}</button>
+            </li>
+          </ul>
+        </div>
+      );
+    };
     
