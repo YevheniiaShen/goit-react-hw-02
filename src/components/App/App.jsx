93d75d07e4f510d3e0d9c197c7d8d5beb835b9e1
Евгения Додиван <evgenita.d97@gmail.com>
2024-02-08 22:59:46 +0100

    try

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,71 @@
-import { Discription } from "../Discription/Discription"
+import css from "./App.module.css";
+import { useEffect, useState } from "react";
+import { Discription } from "../Discription/Discription";
+import { Feedback } from "../Feedback/Feedback";
 import { Options } from "../Options/Options";
-
+import { Notification } from "../Notification/Notification";
 
 export const App = () => {
+  const initState = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  const [state, setState] = useState(
+    () => JSON.parse(window.localStorage.getItem("feedback")) ?? initState
+  );
+
+  const { good, neutral, bad } = state;
+  const total = good + neutral + bad;
+  const positive = Math.round(((good + neutral) / total) * 100);
+
+  const handleClick = (name) => {
+    setState((prev) => ({
+      ...prev,
+      [name]: prev[name] + 1,
+    }));
+  };
+
+  const handleReset = () => {
+    setState(initState);
+  };
+
+  useEffect(() => {
+    window.localStorage.setItem("feedback", JSON.stringify(state));
+  }, [state]);
   return (
-    <div>
+    <>
       <Discription />
-      <Options />
-      {/* <span>Good: {count}</span> */}
-      {/* <Feedback /> */}
-    </div>
+
+      <div className={css.wrap}>
+        <Options
+          handleClick={handleClick}
+          options={Object.keys(initState)}
+        />
+
+        {total > 0 && (
+          <button
+            type="button"
+            onClick={handleReset}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+
+      {total > 0 ? (
+        <Feedback
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positive={positive}
+          // options={[...state, total, positive]}
+        />
+      ) : (
+        <Notification />
+      )}
+    </>
   );
 };
--- src/components/Discription/Discription.jsx
+++ src/components/Discription/Discription.jsx
@@ -1,11 +1,11 @@
 export const Discription = () => {
   return (
-<div>
-<h1>Sip Happens Café</h1>
-<p>
-Please leave your feedback about our service by selecting one of the options below.
-</p>
-
-</div>
- );
+    <>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </>
+  );
 };
